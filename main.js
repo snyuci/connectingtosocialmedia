29e8a351bfe150f4482f8dd23e7d73f24977bda8
snyuci <88171942+snyuci@users.noreply.github.com>
2021-08-14 11:57:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -39,14 +39,14 @@ $(function() {
     settings.condition_1_likes = [12000, 9999999]; 
 
     // In condition 2, user will receive 5 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_2_likes = [10000, 15000,35000,80000,150000];  
+    settings.condition_2_likes = [12000, 15000,35000,80000,150000];  
     
     // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
     settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
 
 	// **Others' likes**     
 	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
-	settings.condition_1_adjusted_likes = [12000, 15000,35000,80000,110000]; // 5
+	settings.condition_1_adjusted_likes = [12000, 35000,80000,110000, 130000]; // 5
 	settings.condition_2_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
 	settings.condition_3_adjusted_likes = [12000, 9999999]; //1	
 	
