d19a2a9bade9ff1b06f346f27d99b853ca96a21e
snyuci <88171942+snyuci@users.noreply.github.com>
2021-08-23 11:26:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -36,10 +36,10 @@ $(function() {
 	// In cases with only 1 "like," a second "like" is added with time point 9999999. This "like" is added for programming purposes and is never executed, as it is outside the task time
 
     // In condition 1, the participant will receive 1 like at the following timepoint (in ms). Default: [12000, 9999999]
-    settings.condition_1_likes = [15000, 9999999]; 
+    settings.condition_1_likes = [20000, 9999999]; 
 
     // In condition 2, user will receive 5 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_2_likes = [15000,35000,80000,150000];  
+    settings.condition_2_likes = [20000,60000,90000,150000];  
     
     // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
     settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
